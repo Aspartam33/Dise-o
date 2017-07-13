fdaffff49b5ee09f00c8354d52d909d594710fd1
Juan Hernandez <juanlh182@gmail.com>
2017-07-12 21:09:11 -0400

    Aqui tengo una prueba de lo que seria la configuración , veanla

--- Calendario.py
+++ Calendario.py
@@ -1,19 +1,12 @@
-import calendar
-#Para el uso del modulo de calendario se necesita el formato aaaa/mm/dd en un string ingresado por el usuario(en la interface)
-class Calendario:
-    #Dada la funcion weekday del modulo calendar se requiere de transformar el numero entero de 0 a 6 a un string para especificar exactamente el día que cae el inicio
-    # y el final de proyecto según lo ingrese el usuario
-    dias={0:'Lunes',1:'Martes',2:'Miercoles',3:'Jueves',4:'Viernes',5:'Sabado',6:'Domingo'}
-    def __init__(self,FechaIniIngre,FechaFinIngre):
-        #Se toman los primeros parametros para realizar el calculo del día que cae el inicio y final según las fechas ingresadas por el usuario.
-        self.InicioP=dias[calendar.weekday(int(FechaIniIngre.split('/')[0]),int(FechaIniIngre.split('/')[1]),int(FechaIniIngre.split('/').[2]))]+' '+FechaIniIngre
-        self.FinP=dias[calendar.weekday(int(FechaFinIngre.split('/')[0]),int(FechaFinIngre.split('/')[1]),int(FechaFinIngre.split('/').[2]))]+' '+FechaFinIngre
-    #Una vez realizado nuestro formateo de la fecha de inicio y proyecto y almacenado en el objeto Calendario procedemos a crear el calendario del proyecto
-    # En este caso será en texto simple pero puede ser usado para creación grafica.
-    def CalendarioTexto(self):
+import sys
+from PyQt5.QtWidgets import (QDialog,QDateEdit,QTextEdit,QDialogButtonBox,QCalendarWidget,QApplication)
+from PyQt5.uic import loadUi
 
-
-
-
-
-
+class Configuracion(QDialog):
+    def __init__(self):
+        QDialog.__init__(self)
+        loadUi("Configuracion.ui",self)
+        FechaI=self.CalendarioFechaI.selectedDate()
+        self.CalendarioFechaF.setSelectedDate(FechaI.addDays(1))
+        self.FechaIcaja.setDate(self.CalendarioFechaI.selectedDate())
+        self.FechaFcaja.setDate(FechaI.addDays(1))
---
+++ PruebaVentanaConfig.pyw
@@ -0,0 +1,20 @@
+from Calendario import Configuracion
+import sys
+from PyQt5.QtWidgets import (QApplication,QMainWindow,QPushButton,QLabel)
+
+class VentanaInicio(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        self.resize(600,600)
+        self.setWindowTitle("EjemplodeVentanaPrincipal")
+        self.BotondeCreacion=QPushButton(self)
+        self.BotondeCreacion.setText("Crear Nuevo Proyecto")
+        self.BotondeCreacion.resize(300,20)
+        self.VentanaEmergente=Configuracion()
+        self.BotondeCreacion.clicked.connect(self.AbrirConfiguracion)
+    def AbrirConfiguracion(self):
+        self.VentanaEmergente.exec_()
+app=QApplication(sys.argv)
+ventana=VentanaInicio()
+ventana.show()
+app.exec_()
